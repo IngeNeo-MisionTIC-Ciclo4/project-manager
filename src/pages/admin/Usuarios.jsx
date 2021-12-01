5a2020e25f9cd7820a9382738dcf617d33b494c6
Diana Pinilla <dianii230416@gmail.com>
2021-12-01 13:32:23 -0500

    creacion de interfazusuarios

--- src/pages/admin/Usuarios.jsx
+++ src/pages/admin/Usuarios.jsx
@@ -4,49 +4,46 @@ import { Link } from 'react-router-dom';
 const Usuarios = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen px-4 py-2 bg-gray-50 sm:px-6 lg:px-8">
-			<h2 className='mt-6 text-3xl font-extrabold text-center text-blue-600'>Modificar usaurios</h2>
+			<h2 className='mt-6 text-3xl font-extrabold text-center text-blue-600'> Usaurios</h2>
 			<form className='mt-8 space-y-6'>
-				<div className='grid grid-cols-2 gap-2 rounded-md shadow-sm'>
+				<div className=' gap-2 rounded-md shadow-sm'>
+					<label htmlFor='cedula'> Cedula
+					<input name='cedula' type='text' required="true"
+							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
+							placeholder='Cedula'/>
+					</label>
 					<label htmlFor='nombre'> Nombre
 					<input name='nombre' type='text' autoComplete='email' required="true"
 							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
-							placeholder='Tu nombre'/>
+							placeholder='Nombre'/>
 					</label>
 					<label htmlFor='apellido'> Apellido
 						<input name='apellido' type='text' required="true"
 							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
-							placeholder='Tu Apellido'/>
+							placeholder='Apellido'/>
 					</label>
-					<label htmlFor='telefono'> Teléfono móvil
-						<input name='telefono' type='tel' required="true"
-							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'
-							placeholder='Tu numero celular' />
-					</label>
-					<label htmlFor='correo'>Correo electrónico
+					<label htmlFor='correo'> Correo electrónico
 						<input name='correo' type='email' required="true"
 							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm' />
 					</label>
-					<label htmlFor='contraseña'> Contraseña
-						<input name='contraseña' type='password' required="true"
-							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm'/>
+					<label htmlFor='rol'> Rol
+						<input name='rol' type='text' required="true"
+							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm' 
+							placeholder='Rol'/>
 					</label>
-					<label htmlFor='contraseña2'> Confirma tu contraseña
-						<input name='contraseña2' type='password' required="true"
-							className='relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm' />
-					</label>
-				</div>
-
-				<div>
-					<button type='submit'className='relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md group hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'>
-						<Link to='#'>Regístrate</Link>
-					</button>
-				</div>
-
-				<div className='flex items-center justify-between'>
-					<span>¿Ya tienes cuenta?</span>
-					<Link to='/login'>
-						<span className='font-medium text-blue-600 hover:text-blue-500'>Inicia Sesión</span>
-					</Link>
+					<label htmlFor="estado" className="flex flex-col"> Estado del usuario
+                    	<select name="estado" 
+                            className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                            required
+                            defaultValue={0}>
+                        	<option>Pendiente</option>
+                        	<option>Autorizado</option>
+							<option>No Autorizado</option>
+                    	</select>
+                	</label>
+					<button type='submit' className='py-4 mt-6 text-white bg-green-400 rounded-full shadow-md hover:bg-green-600'>
+                    	Guardar Usuario
+                	</button>
 				</div>
 			</form>
 		</div>
