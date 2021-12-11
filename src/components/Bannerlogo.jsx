073e1317823d47a01ab4c838862793fd9eb34e87
Javier Romero <javier.rom1911@gmail.com>
2021-12-11 00:48:36 -0500

    Se actualiza tamaño Logo

--- src/components/Bannerlogo.jsx
+++ src/components/Bannerlogo.jsx
@@ -4,7 +4,7 @@ import Imgbannerlogo from "../media/Logo-uni.png";
 const Bannerlogo = ({children}) => {
     return (
         <div className="flex flex-col items-center w-full bg-white">
-            <img src={Imgbannerlogo} alt="Bannerlogo" className='w-20 h-20'></img>
+            <img src={Imgbannerlogo} alt="Bannerlogo" className='m-4 w-44 h-44'></img>
         </div>
     );
 }
